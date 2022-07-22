8bbab3e123b3a68864198d5ca33ae2b2dad38083
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-07-22 14:12:04 +0530

    updated MINMAX BACKTRACKING ALGO

--- Code/Tic_Tac_Toe.cpp
+++ Code/Tic_Tac_Toe.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define SIDE 3
 #define COMPUTERMOVE 'O'
 #define HUMANMOVE 'X'
+
 void showBoard(char board[][SIDE])
 {
 		cout<<"\t\t\t "<<board[0][0]<<" | " <<board[0][1]<<" | "<<board[0][2] <<"\n"; 
@@ -29,25 +30,25 @@ void initialise(char board[][SIDE])
 // Initially the board to '*' as said
 		for (int i=0; i<SIDE; i++)
 		{
-		for (int j=0; j<SIDE; j++)
-		board[i][j] = '*';
-		}
+			for (int j=0; j<SIDE; j++)
+				board[i][j] = '*';
+		}	
 }
 
 void declareWinner(int whoseTurn)
 {
 		if (whoseTurn == COMPUTER)
-		printf("COMPUTER has won\n");
+			cout<<"COMPUTER has won\n";
 		else
-		printf("HUMAN has won\n");
+			cout<<"HUMAN has won\n";
 }
 
 bool rowCrossed(char board[][SIDE])
 {
 		for (int i=0; i<SIDE; i++)
 		{
-		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '*')
-		return (true);
+			if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '*')
+				return (true);
 		}
 		return(false);
 }
@@ -55,8 +56,8 @@ bool columnCrossed(char board[][SIDE])
 {
 		for (int i=0; i<SIDE; i++)
 		{
-		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != '*')
-		return (true);
+			if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != '*')
+				return (true);
 		}
 		return(false);
 }
@@ -64,10 +65,10 @@ bool columnCrossed(char board[][SIDE])
 bool diagonalCrossed(char board[][SIDE])
 {
 		if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '*')
-		return(true);
+			return(true);
 		
 		if (board[0][2] == board[1][1] &&board[1][1] == board[2][0] && board[0][2] != '*')
-		return(true);
+			return(true);
 		
 		return(false);
 }
